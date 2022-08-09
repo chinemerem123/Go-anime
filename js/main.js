b1fa4bf9fe12d0d77fafef7e0c471382a1b967e4
chinemerem123 <chrisnnaji443@gmail.com>
2022-08-09 14:55:14 -0700

    newly-done

--- js/main.js
+++ js/main.js
@@ -1,3 +1,36 @@
 setInterval(()=>{
 document.querySelector(".load").style.display="none";
 }, 2000);
+
+let quoteTitle=document.getElementById("quote-input-text1");
+let titleBtn=document.getElementById("get-quote-title");
+let anime=document.getElementById("anime");
+let quoteText=document.getElementById("qoute-text");
+let charater=document.getElementById("charater");
+
+var index = Math.floor(Math.random() * 10) + 0;
+
+
+titleBtn.addEventListener('click', ()=>{
+    fetch(`https://animechan.vercel.app/api/quotes/anime?title=${quoteTitle.value}`)
+      .then(response => response.json())
+      .then(quotes => {
+        if(quotes.length == 0){
+            alert("Nothing Found!!");
+         }
+        anime.innerText=quotes[index].anime;
+        quoteText.innerText=quotes[index].quote;
+        charater.innerText=quotes[index].character;
+      })
+})
+
+document.getElementById("clear1").addEventListener('click',()=>{
+    anime.innerText="";
+    quoteTitle.value="";
+    quoteText.innerText="";
+    charater.innerText="";
+})
+
+      /*fetch('https://animechan.vercel.app/api/available/anime')
+      .then(response => response.json())
+      .then(anime => console.log(anime))*/
